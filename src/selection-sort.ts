24d3480aec7d6bd3d53d811d9947c9c4be8866ea
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-25 11:09:25 -0300

    refactor(selection-sort): improve implementation

--- src/selection-sort.ts
+++ src/selection-sort.ts
@@ -1,31 +1,25 @@
 import { time, timeEnd } from "console";
+import { List } from "./types";
+import { getUnsortedList } from "./get-unsorted-list";
 
-type List = Array<string | number>;
+function selectionSort(list: List) {
+  for (let i = 0; i < list.length; i++) {
+    let minIndex = i;
 
-function selectionSort(list: List, startIndex: number) {
-  if (!list.length) return list;
-
-  const sortedList: List = [...list];
-
-  for (let i = startIndex; i < list.length; i++) {
-    const current = sortedList[i];
-
-    if (current < sortedList[startIndex]) {
-      const temp = sortedList[startIndex];
-      sortedList[startIndex] = current;
-      sortedList[i] = temp;
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[minIndex] > list[j]) {
+        minIndex = j;
+      }
     }
-  }
 
-  if (startIndex == list.length - 1) return list;
-  return selectionSort(sortedList, startIndex + 1);
+    const temp = list[i];
+    list[i] = list[minIndex];
+    list[minIndex] = temp;
+  }
 }
 
-const unsortedList = Array(100)
-  .fill(0)
-  .map(() => Math.round(Math.random() * 999));
-
-time("sort-function");
-const sortedList = selectionSort(unsortedList, 0);
-timeEnd("sort-function");
-console.log(sortedList);
+const list = getUnsortedList(100);
+time("selection-sort");
+selectionSort(list);
+timeEnd("selection-sort");
+console.log(list);
